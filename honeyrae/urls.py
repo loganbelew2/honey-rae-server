a432a7692c1e5cc218546ee0928645ac224d9981
Logan Belews <Loganbelew2@gmail.com>
2023-08-09 11:09:23 -0500

    added models and migrated seed data

--- honeyrae/urls.py
+++ honeyrae/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from repairsapi.views import register_user, login_user, CustomerView
+from rest_framework import routers
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'customers', CustomerView, 'customer')
 
 urlpatterns = [
+    path('register', register_user),
+    path('login', login_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
